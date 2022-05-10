d6358a41c28dfaa99a4d7678d1cf2907056e1c7e
Zing <zingimmick@outlook.com>
2022-05-10 21:40:49 +0800

    Add support for new ECSConfig and RectorConfig class (#41)

--- ecs.php
+++ ecs.php
@@ -2,16 +2,14 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Zing\CodingStandard\Set\ECSSetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(ECSSetList::PHP_72);
-    $containerConfigurator->import(ECSSetList::CUSTOM);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([ECSSetList::PHP_72, ECSSetList::CUSTOM]);
+    $parameters = $ecsConfig->parameters();
+    $ecsConfig->parallel();
     $parameters->set(
         Option::PATHS,
         [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
