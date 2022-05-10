4e56c0eda9742687355a89f06c3574f4e2cedeff
zingimmick <zingimmick@outlook.com>
2022-05-10 21:37:07 +0800

    Add support for new ECSConfig and RectorConfig class

--- ecs.php
+++ ecs.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Zing\CodingStandard\Set\ECSSetList;
-
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(ECSSetList::PHP_72);
-    $containerConfigurator->import(ECSSetList::CUSTOM);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
+use \Symplify\EasyCodingStandard\Config\ECSConfig;
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([ECSSetList::PHP_72, ECSSetList::CUSTOM]);
+    $parameters = $ecsConfig->parameters();
+    $ecsConfig->parallel();
     $parameters->set(
         Option::PATHS,
         [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
--- rector.php
+++ rector.php
@@ -14,28 +14,18 @@ use Rector\Set\ValueObject\LevelSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Zing\CodingStandard\Set\RectorSetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(RectorSetList::CUSTOM);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_72);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, __DIR__ . '/phpstan.neon');
-    $parameters->set(Option::BOOTSTRAP_FILES, [__DIR__ . '/vendor/nunomaduro/larastan/bootstrap.php']);
-    $parameters->set(
-        Option::SKIP,
-        [
-            RenameVariableToMatchMethodCallReturnTypeRector::class,
-            RenameParamToMatchTypeRector::class,
-            AddSeeTestAnnotationRector::class,
-            FinalizeClassesWithoutChildrenRector::class,
-            RepeatedLiteralToClassConstantRector::class,
-            PrivatizeLocalGetterToPropertyRector::class,
-            \Rector\TypeDeclaration\Rector\FunctionLike\ParamTypeDeclarationRector::class,
-        ]
-    );
-    $parameters->set(
-        Option::PATHS,
-        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
-    );
+return static function (\Rector\Config\RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_72,  PHPUnitSetList::PHPUNIT_CODE_QUALITY,RectorSetList::CUSTOM, ]);
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
+    $rectorConfig->bootstrapFiles([__DIR__ . '/vendor/nunomaduro/larastan/bootstrap.php']);
+    $rectorConfig->skip([
+        RenameVariableToMatchMethodCallReturnTypeRector::class,
+        RenameParamToMatchTypeRector::class,
+        AddSeeTestAnnotationRector::class,
+        FinalizeClassesWithoutChildrenRector::class,
+        RepeatedLiteralToClassConstantRector::class,
+        PrivatizeLocalGetterToPropertyRector::class,
+        \Rector\TypeDeclaration\Rector\FunctionLike\ParamTypeDeclarationRector::class,
+    ]);
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
 };
