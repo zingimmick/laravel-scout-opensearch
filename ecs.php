4432a1a7264601a48812b8ebb1520da7295ff917
zingimmick <zingimmick@users.noreply.github.com>
2022-05-15 13:05:08 +0000

    Rector Rectify

--- ecs.php
+++ ecs.php
@@ -3,14 +3,10 @@
 declare(strict_types=1);
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 use Zing\CodingStandard\Set\ECSSetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_72, ECSSetList::CUSTOM]);
     $ecsConfig->parallel();
-    $ecsConfig->paths(
-   
-        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
-    );
+    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
 };
