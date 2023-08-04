0bc27f34c244b9c21c164c49613685a050568b86
Zing <zingimmick@outlook.com>
2023-08-04 17:26:23 +0800

    Update ecs.php

--- ecs.php
+++ ecs.php
@@ -7,6 +7,5 @@ use Zing\CodingStandard\Set\ECSSetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_80, ECSSetList::CUSTOM]);
-    $ecsConfig->parallel();
-    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
+    $ecsConfig->parallel();$ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
 };
