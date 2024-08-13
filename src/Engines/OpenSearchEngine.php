002f9a1f19287df52f5b2226bf4c50fb6ce4104c
Zing <zingimmick@outlook.com>
2024-08-13 23:02:01 +0800

    Update OpenSearchEngine.php

--- src/Engines/OpenSearchEngine.php
+++ src/Engines/OpenSearchEngine.php
@@ -30,7 +30,7 @@ class OpenSearchEngine extends Engine
     /**
      * Update the given model in the index.
      *
-     * @param \Illuminate\Database\Eloquent\Collection $models
+     * @param \Illuminate\Database\Eloquent\Collection<int, \Illuminate\Database\Eloquent\Model> $models
      */
     public function update($models): void
     {
@@ -44,16 +44,17 @@ class OpenSearchEngine extends Engine
             $models->each->pushSoftDeleteMetadata();
         }
 
-        $objects = $models->map(static function ($model): ?array {
+        $objects = $models->map(static function ($model): array {
             $searchableData = $model->toSearchableArray();
             if (empty($searchableData)) {
-                return null;
+                return [];
             }
 
             return array_merge($searchableData, $model->scoutMetadata(), [
                 $model->getScoutKeyName() => $model->getScoutKey(),
             ]);
-        })->filter()
+        })
+            ->filter()
             ->values()
             ->all();
 
@@ -79,7 +80,7 @@ class OpenSearchEngine extends Engine
     /**
      * Remove the given model from the index.
      *
-     * @param \Illuminate\Database\Eloquent\Collection $models
+     * @param \Illuminate\Database\Eloquent\Collection<int, \Illuminate\Database\Eloquent\Model> $models
      */
     public function delete($models): void
     {
@@ -203,6 +204,8 @@ class OpenSearchEngine extends Engine
      * Pluck and return the primary keys of the given results.
      *
      * @param array{hits: mixed[]|null}|null $results
+     *
+     * @return \Illuminate\Support\Collection<int, int|string>
      */
     public function mapIds($results): Collection
     {
@@ -219,7 +222,7 @@ class OpenSearchEngine extends Engine
      * @param array{hits: mixed[]|null}|null $results
      * @param \Illuminate\Database\Eloquent\Model $model
      *
-     * @return \Illuminate\Database\Eloquent\Collection
+     * @return \Illuminate\Database\Eloquent\Collection<int, \Illuminate\Database\Eloquent\Model>
      */
     public function map(Builder $builder, $results, $model): mixed
     {
@@ -249,6 +252,8 @@ class OpenSearchEngine extends Engine
      *
      * @param array{hits: mixed[]|null}|null $results
      * @param \Illuminate\Database\Eloquent\Model $model
+     *
+     * @return \Illuminate\Support\LazyCollection<int, \Illuminate\Database\Eloquent\Model>
      */
     public function lazyMap(Builder $builder, $results, $model): LazyCollection
     {
