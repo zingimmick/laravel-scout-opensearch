635944a6833d0607b900090d3fab0643394cc5c4
Zing <zingimmick@outlook.com>
2026-02-24 11:10:44 +0800

    Refactor configuration setup in OpenSearchEngineTest

--- tests/OpenSearchEngineTest.php
+++ tests/OpenSearchEngineTest.php
@@ -30,8 +30,8 @@ final class OpenSearchEngineTest extends TestCase
 
     protected function setUp(): void
     {
-        Config::shouldReceive('get')->with('scout.after_commit', m::any())->andReturn(false);
-        Config::shouldReceive('get')->with('scout.soft_delete', m::any())->andReturn(false);
+        Config::set('scout.after_commit', false);
+        Config::set('scout.soft_delete', false);
     }
 
     public function testUpdateAddsObjectsToIndex(): void
