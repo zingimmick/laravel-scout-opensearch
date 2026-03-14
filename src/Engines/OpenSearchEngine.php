cfcb996fe0a5f0019a21b8bbc4bafe61db1e9976
Zing <zingimmick@outlook.com>
2026-03-14 22:21:10 +0800

    Add search engine meta data to results (#130)

--- src/Engines/OpenSearchEngine.php
+++ src/Engines/OpenSearchEngine.php
@@ -277,6 +277,17 @@ class OpenSearchEngine extends Engine
 
         return $model->getScoutModelsByIds($builder, $objectIds)
             ->filter(static fn ($model): bool => \in_array($model->getScoutKey(), $objectIds, false))
+            ->map(static function ($model) use ($results, $objectIdPositions) {
+                $result = $results['hits'][$objectIdPositions[$model->getScoutKey()]] ?? [];
+
+                foreach ($result as $key => $value) {
+                    if (str_starts_with($key, '_')) {
+                        $model->withScoutMetadata($key, $value);
+                    }
+                }
+
+                return $model;
+            })
             ->sortBy(static fn ($model): int => $objectIdPositions[$model->getScoutKey()])->values();
     }
 
@@ -309,6 +320,17 @@ class OpenSearchEngine extends Engine
         return $model->queryScoutModelsByIds($builder, $objectIds)
             ->cursor()
             ->filter(static fn ($model): bool => \in_array($model->getScoutKey(), $objectIds, false))
+            ->map(static function ($model) use ($results, $objectIdPositions) {
+                $result = $results['hits'][$objectIdPositions[$model->getScoutKey()]] ?? [];
+
+                foreach ($result as $key => $value) {
+                    if (str_starts_with($key, '_')) {
+                        $model->withScoutMetadata($key, $value);
+                    }
+                }
+
+                return $model;
+            })
             ->sortBy(static fn ($model): int => $objectIdPositions[$model->getScoutKey()])->values();
     }
 
--- tests/OpenSearchEngineTest.php
+++ tests/OpenSearchEngineTest.php
@@ -500,6 +500,9 @@ final class OpenSearchEngineTest extends TestCase
         ], $model);
 
         $this->assertCount(1, $results);
+        $this->assertSame([
+            '_id' => 1,
+        ], $results->first()->scoutMetadata());
     }
 
     public function testLazyMapMethodRespectsOrder(): void
