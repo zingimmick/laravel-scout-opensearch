6c0d84ee13c22f3e3c041841da107a8a11365b17
Zing <zingimmick@outlook.com>
2026-09-08 11:26:50 +0800

    Refactor scoutMetadata assertions in tests

--- tests/OpenSearchEngineTest.php
+++ tests/OpenSearchEngineTest.php
@@ -402,9 +402,10 @@ final class OpenSearchEngineTest extends TestCase
         ], $model);
 
         $this->assertCount(1, $results);
+        $metadata = $results->first()->scoutMetadata();
         $this->assertSame([
             '_id' => 1,
-        ], $results->first()->scoutMetadata());
+        ], $metadata);
     }
 
     public function testMapMethodRespectsOrder(): void
@@ -503,9 +504,10 @@ final class OpenSearchEngineTest extends TestCase
         ], $model);
 
         $this->assertCount(1, $results);
+        $metadata = $results->first()->scoutMetadata();
         $this->assertSame([
             '_id' => 1,
-        ], $results->first()->scoutMetadata());
+        ], $metadata);
     }
 
     public function testLazyMapMethodRespectsOrder(): void
